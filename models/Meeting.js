261451916248cc406dbad258ff9700ea10c9b6c8
NapXDD <19520631@gm.uit.edu.vn>
2023-05-28 10:19:09 +0700

    update type for meeting

--- models/Meeting.js
+++ models/Meeting.js
@@ -1,26 +1,24 @@
 const mongoose = require("mongoose");
 
-const meetingSchema = mongoose.Schema(
-    {
-        roomName: {
-            type: String,
-            max: 10,
-            require: true
-        },
-        dateTime: {
-            type: Date,
-            require: true
-        },
-        requestBy: {
-            type: String,
-            require: true
-        },
-        approveStatus: { 
-            type: Boolean,
-            require: true,
-            default: false
-        }
-    }
-)
+const meetingSchema = mongoose.Schema({
+  roomName: {
+    type: String,
+    max: 10,
+    require: true,
+  },
+  dateTime: {
+    type: Date,
+    require: true,
+  },
+  requestBy: {
+    type: String,
+    require: true,
+  },
+  approveStatus: {
+    type: Boolean,
+    require: true,
+    default: false,
+  },
+});
 
 module.exports = mongoose.model("meeting", meetingSchema);
