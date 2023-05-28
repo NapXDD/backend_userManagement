15a1c08db99ca97878dc4adacb2517736107541e
NapXDD <19520631@gm.uit.edu.vn>
2023-05-28 10:19:02 +0700

    update type for document

--- models/Documents.js
+++ models/Documents.js
@@ -1,27 +1,29 @@
 const mongoose = require("mongoose");
 
-const docsSchema = mongoose.Schema(
-    {
-        docName: {
-            type: String,
-            max: 60,
-            require: true,
-            unique: true
-        },
-        description: {
-            type: String,
-            max: 100,
-            require: true
-        },
-        submitDate: {
-            type: Date
-        },
-        approveStatus: { 
-            type: Boolean,
-            require: true,
-            default: false
-        }
-    }
-)
+const docsSchema = mongoose.Schema({
+  docName: {
+    type: String,
+    max: 60,
+    require: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    max: 100,
+    require: true,
+  },
+  submitDate: {
+    type: Date,
+  },
+  approveStatus: {
+    type: Boolean,
+    require: true,
+    default: false,
+  },
+  uploadBy: {
+    type: String,
+    require: true,
+  },
+});
 
 module.exports = mongoose.model("docs", docsSchema);
